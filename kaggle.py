f77beb40a720dd01356f6ffc5935cc7dc20a3794
Bilal Ahmed <bilallahmedd685@gmail.com>
2026-09-25 11:46:39 +0500

    Clarify comment for dataset download

--- kaggle.py
+++ kaggle.py
@@ -1,6 +1,6 @@
 import kagglehub
 
-# Download latest version
+# Download latest version of this dataset
 path = kagglehub.dataset_download("debayank2024/fifa-2026-player-performance")
 
 print("Path to dataset files:", path)
